71c20a22c3c60b61b29fa2f0b9431abd4c1ea720
Aditya Subono <adityasubono@gmail.com>
2021-08-14 17:46:29 +0700

    Update Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,15 +3,15 @@ import React from "react";
 const Modal = ({ visible, toggle, content, title }) => visible ? (
   <div className="modal" style={{ display: 'block' }} data-testid="modal-1">
      <div className="modal-dialog" role="document" style={{ zIndex: 100 }}>
-      <div className="modal-content">
-        <div className="modal-header">
-          <h5 className="modal-title" id="exampleModalLabel">{title}</h5>
+      <div className="modal-content border-0">
+        <div className="modal-header bg-dark border-0">
+          <h5 className="modal-title text-warning" id="exampleModalLabel">{title}</h5>
         </div>
-        <div className="modal-body" data-testid="modal-body">
+        <div className="modal-body bg-dark border-0" data-testid="modal-body">
           {content}
         </div>
-        <div className="modal-footer">
-          <button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={toggle}>Close</button>
+        <div className="modal-footer bg-dark border-0">
+          <button type="button" className="btn btn-warning" data-dismiss="modal" onClick={toggle}>Close</button>
         </div>
       </div>
     </div>
